2634c9d2a3942e2ec460577b1db62a92ed49ed1f
Alex Saxena <saxenaalex@gmail.com>
2024-12-15 12:44:36 +0100

    Adds 6 Cards

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,62 @@ function App() {
       <h1 className="text-orange-600">Memory Game!</h1>
       <div id="Card-Container" className="p-5">
         <CardComponent
+          cardId={1}
+          imgSrc="https://picsum.photos/200"
+          isFlipped={true}
+          isMatched={false}
+          onClick={() => {
+            console.log("Card Pressed with ID -> ", 1);
+          }}
+        />
+
+        <CardComponent
           cardId={2}
-          imgSrc="d"
+          imgSrc="Image"
           isFlipped={false}
           isMatched={false}
           onClick={() => {
-            console.log("Card Pressed");
+            console.log("Card Pressed with ID -> ", 2);
+          }}
+        />
+
+        <CardComponent
+          cardId={3}
+          imgSrc="Image"
+          isFlipped={false}
+          isMatched={false}
+          onClick={() => {
+            console.log("Card Pressed with ID -> ", 3);
+          }}
+        />
+
+        <CardComponent
+          cardId={4}
+          imgSrc="Image"
+          isFlipped={false}
+          isMatched={false}
+          onClick={() => {
+            console.log("Card Pressed with ID -> ", 4);
+          }}
+        />
+
+        <CardComponent
+          cardId={5}
+          imgSrc="Image"
+          isFlipped={false}
+          isMatched={false}
+          onClick={() => {
+            console.log("Card Pressed with ID -> ", 5);
+          }}
+        />
+
+        <CardComponent
+          cardId={6}
+          imgSrc="Image"
+          isFlipped={false}
+          isMatched={false}
+          onClick={() => {
+            console.log("Card Pressed with ID -> ", 6);
           }}
         />
       </div>
