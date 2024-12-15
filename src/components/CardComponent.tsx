0b425819abfd42609161e0fb49588ff8c408f427
Alex Saxena <saxenaalex@gmail.com>
2024-12-15 10:37:06 +0100

    Updates from arrow function to regular function for ease of use with types & adds props for flipped & matched

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,16 +1,40 @@
-const CardComponent = () => {
+type CardProps = {
+  cardId: number;
+  imgSrc: string;
+  isFlipped: boolean;
+  isMatched: boolean;
+  onClick: () => void;
+};
+
+function CardComponent({
+  cardId,
+  imgSrc,
+  isFlipped,
+  isMatched,
+  onClick,
+}: CardProps) {
   return (
-    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow">
+    <div
+      className={`max-w-xs bg-white border border-gray-200 rounded-lg shadow ${
+        isMatched ? "opacity-50" : ""
+      }`}
+      onClick={onClick}
+    >
       <div className="aspect-w-4 aspect-h-3">
-        <img
-          className="w-full h-full object-cover rounded-lg"
-          src="https://via.placeholder.com/400x300"
-          alt="Memory Card"
-          id="#"
-        />
+        {isFlipped || isMatched ? (
+          <img
+            className="w-full h-full object-cover rounded-lg"
+            src={imgSrc}
+            alt={`Card ${cardId}`}
+          />
+        ) : (
+          <div className="w-full h-full bg-gray-300 rounded-lg flex items-center justify-center">
+            <p className="text-gray-700 text-xl">?</p>
+          </div>
+        )}
       </div>
     </div>
   );
-};
+}
 
 export default CardComponent;
