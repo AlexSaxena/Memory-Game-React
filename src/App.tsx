f28426312a8fee774e1f72e8c117b6bd531a0f0a
Alex Saxena <saxenaalex@gmail.com>
2024-12-15 14:40:42 +0100

    Refactors cards into an array & maps into CardComponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,36 @@ import "./App.css";
 import CardComponent from "./components/CardComponent";
 
 function App() {
+  const cardContentArray = [
+    {
+      cardId: 1,
+      imgSrc: "/house.jpg",
+      isFlipped: true,
+      isMatched: false,
+      onClick: () => {
+        console.log("Card Pressed ");
+      },
+    },
+    {
+      cardId: 2,
+      imgSrc: "/space.jpg",
+      isFlipped: true,
+      isMatched: false,
+      onClick: () => {
+        console.log("Card Pressed ");
+      },
+    },
+    {
+      cardId: 3,
+      imgSrc: "/space_pod.jpg",
+      isFlipped: true,
+      isMatched: false,
+      onClick: () => {
+        console.log("Card Pressed ");
+      },
+    },
+  ];
+
   return (
     <>
       <main className="min-h-screen flex flex-col items-center justify-start overflow-y-auto">
@@ -13,65 +43,18 @@ function App() {
             id="Card-Container"
             className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center"
           >
-            <CardComponent
-              cardId={1}
-              imgSrc="/house.jpg"
-              isFlipped={true}
-              isMatched={false}
-              onClick={() => {
-                console.log("Card Pressed with ID -> ", 1);
-              }}
-            />
-
-            <CardComponent
-              cardId={2}
-              imgSrc="/space.jpg"
-              isFlipped={true}
-              isMatched={false}
-              onClick={() => {
-                console.log("Card Pressed with ID -> ", 2);
-              }}
-            />
-
-            <CardComponent
-              cardId={3}
-              imgSrc="/space_pod.jpg"
-              isFlipped={true}
-              isMatched={false}
-              onClick={() => {
-                console.log("Card Pressed with ID -> ", 3);
-              }}
-            />
-
-            <CardComponent
-              cardId={4}
-              imgSrc="Image"
-              isFlipped={false}
-              isMatched={false}
-              onClick={() => {
-                console.log("Card Pressed with ID -> ", 4);
-              }}
-            />
-
-            <CardComponent
-              cardId={5}
-              imgSrc="Image"
-              isFlipped={false}
-              isMatched={false}
-              onClick={() => {
-                console.log("Card Pressed with ID -> ", 5);
-              }}
-            />
-
-            <CardComponent
-              cardId={6}
-              imgSrc="Image"
-              isFlipped={false}
-              isMatched={false}
-              onClick={() => {
-                console.log("Card Pressed with ID -> ", 6);
-              }}
-            />
+            {cardContentArray.map((card) => {
+              return (
+                <CardComponent
+                  key={card.cardId}
+                  cardId={card.cardId}
+                  imgSrc={card.imgSrc}
+                  isFlipped={card.isFlipped}
+                  isMatched={card.isMatched}
+                  onClick={card.onClick}
+                />
+              );
+            })}
           </div>
         </div>
       </main>
